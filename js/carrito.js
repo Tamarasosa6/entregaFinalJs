25fd0a761493bdcf7ed7afdc5a2a991c51fa06ca
Tamarasosa6 <tamisosa6@gmail.com>
2024-10-03 03:37:17 -0300

    iniciot

--- js/carrito.js
+++ js/carrito.js
@@ -1,140 +1,147 @@
 let carrito = []; 
 let total = 0;
 
-document.getElementById('btnCarrito').addEventListener('click', () => {
-  const myModal = new bootstrap.Modal(document.getElementById('modalCarrito'));
-  myModal.show();
+document.addEventListener('DOMContentLoaded', () => {
+    const btnCarrito = document.getElementById('btnCarrito');
+    if (btnCarrito) {
+        btnCarrito.addEventListener('click', () => {
+            const myModal = new bootstrap.Modal(document.getElementById('modalCarrito'));
+            myModal.show();
+        });
+    }
+
+    cargarProductos();
+
+    const btnFinalizarCompra = document.getElementById('btnFinalizarCompra');
+    if (btnFinalizarCompra) {
+        btnFinalizarCompra.addEventListener('click', () => {
+            if (carrito.length === 0) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Carrito vacío',
+                    text: 'No hay productos en el carrito para finalizar la compra.',
+                    confirmButtonText: 'Aceptar'
+                });
+            } else {
+                document.getElementById('formCompra').style.display = 'block';
+            }
+        });
+    }
 });
 
 function cargarProductos() {
-  fetch('./json/productos.json')
-    .then(response => response.json())
-    .then(productos => {
-      const contenedorProductos = document.getElementById('productosContenedor');
-      if (contenedorProductos) {
-        contenedorProductos.innerHTML = productos.map(producto => `
-          <div class="producto">
-            <img src="${producto.imagen}" alt="${producto.nombre}">
-            <h3>${producto.nombre}</h3>
-            <p>Precio: $${producto.precio}</p>
-            <input type="number" id="cantidad-${producto.id}" placeholder="Cantidad" min="1" max="${producto.cantidad}">
-            <button onclick="agregarAlCarrito(${producto.id})">Agregar al carrito</button>
-          </div>
-        `).join('');
-      }
-    })
-    .catch(error => console.error('Error al cargar los productos:', error));
+    fetch('./json/productos.json')
+        .then(response => response.json())
+        .then(productos => {
+            const contenedorProductos = document.getElementById('productosContenedor');
+            if (contenedorProductos) {
+                contenedorProductos.innerHTML = productos.map(producto => `
+                    <div class="producto">
+                        <img src="${producto.imagen}" alt="${producto.nombre}">
+                        <h3>${producto.nombre}</h3>
+                        <p>Precio: $${producto.precio}</p>
+                        <input type="number" id="cantidad-${producto.id}" placeholder="Cantidad" min="1" max="${producto.cantidad}">
+                        <button onclick="agregarAlCarrito(${producto.id})">Agregar al carrito</button>
+                    </div>
+                `).join('');
+            }
+        })
+        .catch(error => console.error('Error al cargar los productos:', error));
 }
 
 function agregarAlCarrito(id) {
-  fetch('./json/productos.json')
-    .then(response => response.json())
-    .then(productos => {
-      const producto = productos.find(p => p.id === id);
-      if (producto) {
-        const cantidad = parseInt(document.getElementById(`cantidad-${id}`).value);
-        if (!isNaN(cantidad) && cantidad > 0 && cantidad <= producto.cantidad) {
-          const productoEnCarrito = carrito.find(item => item.id === id);
-          if (productoEnCarrito) {
-            productoEnCarrito.cantidad += cantidad;
-          } else {
-            carrito.push({ ...producto, cantidad });
-          }
-          actualizarCarrito();
-          mostrarNotificacion(`Agregado al carrito: ${producto.nombre} (Cantidad: ${cantidad})`);
-        } else {
-          Swal.fire({
-            icon: 'warning',
-            title: 'Cantidad no válida',
-            text: 'Por favor, introduce una cantidad válida.',
-            confirmButtonText: 'Aceptar'
-          });
-        }
-      }
-    });
+    fetch('./json/productos.json')
+        .then(response => response.json())
+        .then(productos => {
+            const producto = productos.find(p => p.id === id);
+            if (producto) {
+                const cantidad = parseInt(document.getElementById(`cantidad-${id}`).value);
+                if (!isNaN(cantidad) && cantidad > 0 && cantidad <= producto.cantidad) {
+                    const productoEnCarrito = carrito.find(item => item.id === id);
+                    if (productoEnCarrito) {
+                        productoEnCarrito.cantidad += cantidad;
+                    } else {
+                        carrito.push({ ...producto, cantidad });
+                    }
+                    actualizarCarrito();
+                    mostrarNotificacion(`Agregado al carrito: ${producto.nombre} (Cantidad: ${cantidad})`);
+                } else {
+                    Swal.fire({
+                        icon: 'warning',
+                        title: 'Cantidad no válida',
+                        text: 'Por favor, introduce una cantidad válida.',
+                        confirmButtonText: 'Aceptar'
+                    });
+                }
+            }
+        });
 }
 
 function mostrarNotificacion(mensaje) {
-  Swal.fire({
-    icon: 'success',
-    title: 'Producto agregado',
-    text: mensaje,
-    confirmButtonText: 'Aceptar',
-    timer: 2000, 
-    timerProgressBar: true,
-    didOpen: () => {
-      Swal.showLoading();
-    },
-    willClose: () => {
-      Swal.stopTimer();
-    }
-  });
+    Swal.fire({
+        icon: 'success',
+        title: 'Producto agregado',
+        text: mensaje,
+        confirmButtonText: 'Aceptar',
+        timer: 2000, 
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+        willClose: () => {
+            Swal.stopTimer();
+        }
+    });
 }
 
 function actualizarCarrito() {
-  const listaCarrito = document.getElementById('listaCarrito');
-  if (listaCarrito) {
-    listaCarrito.innerHTML = carrito.map(item => `
-      <div>
-        ${item.nombre} - $${item.precio} x ${item.cantidad}
-        <button onclick="eliminarDelCarrito(${item.id})">Eliminar</button>
-      </div>
-    `).join('');
-    total = carrito.reduce((acc, item) => acc + (item.precio * item.cantidad), 0);
-    document.getElementById('carritoTotal').innerText = `Total: $${total}`;
-  }
+    const listaCarrito = document.getElementById('listaCarrito');
+    if (listaCarrito) {
+        listaCarrito.innerHTML = carrito.map(item => `
+            <div>
+                ${item.nombre} - $${item.precio} x ${item.cantidad}
+                <button onclick="eliminarDelCarrito(${item.id})">Eliminar</button>
+            </div>
+        `).join('');
+        total = carrito.reduce((acc, item) => acc + (item.precio * item.cantidad), 0);
+        document.getElementById('carritoTotal').innerText = `Total: $${total}`;
+    }
 
-  document.getElementById('carrito-icon').innerText = carrito.length;
+    document.getElementById('carrito-icon').innerText = carrito.length;
 }
 
 function eliminarDelCarrito(id) {
-  carrito = carrito.filter(item => item.id !== id);
-  actualizarCarrito();
+    carrito = carrito.filter(item => item.id !== id);
+    actualizarCarrito();
 }
 
-document.getElementById('btnFinalizarCompra').addEventListener('click', () => {
-  if (carrito.length === 0) {
-    Swal.fire({
-      icon: 'warning',
-      title: 'Carrito vacío',
-      text: 'No hay productos en el carrito para finalizar la compra.',
-      confirmButtonText: 'Aceptar'
-    });
-  } else {
-    document.getElementById('formCompra').style.display = 'block';
-  }
-});
-
 function finalizarCompra() {
-  const nombre = document.getElementById('nombre').value;
+  const nombre = document.getElementById('nombreCompra').value; // Cambiar a 'nombreCompra'
   const apellido = document.getElementById('apellido').value;
   const email = document.getElementById('email').value;
 
   if (nombre && apellido && email) {
-    Swal.fire({
-      icon: 'success',
-      title: 'Compra exitosa',
-      text: 'Gracias por tu compra!',
-      confirmButtonText: 'Aceptar'
-    }).then(() => {
-      carrito = [];
-      total = 0;
-      actualizarCarrito();
-      document.getElementById('formCompra').style.display = 'none';
+      Swal.fire({
+          icon: 'success',
+          title: 'Compra exitosa',
+          text: 'Gracias por tu compra!',
+          confirmButtonText: 'Aceptar'
+      }).then(() => {
+          carrito = [];
+          total = 0;
+          actualizarCarrito();
+          document.getElementById('formCompra').style.display = 'none';
 
-      
-      setTimeout(() => {
-        window.location.href = 'index.html'; 
-      }, 3000); 
-    });
+          setTimeout(() => {
+              window.location.href = 'index.html'; 
+          }, 3000); 
+      });
   } else {
-    Swal.fire({
-      icon: 'error',
-      title: 'Error',
-      text: 'Por favor, completa todos los campos.',
-      confirmButtonText: 'Aceptar'
-    });
+      Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Por favor, completa todos los campos.',
+          confirmButtonText: 'Aceptar'
+      });
   }
 }
-
-cargarProductos();
--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,50 @@
-document.getElementById('menuDropdown').addEventListener('change', (event) => {
-  const value = event.target.value;
-  if (value === 'Todos') {
-      mostrarProductos(productosGlobales);
-  } else if (value === 'Remeras') {
-      mostrarProductos(productosGlobales.filter(producto => producto.categoria === 'Remeras'));
-  } else if (value === 'Buzos') {
-      mostrarProductos(productosGlobales.filter(producto => producto.categoria === 'Buzos'));
-  } else if (value === 'Camisetas') {
-      mostrarProductos(productosGlobales.filter(producto => producto.categoria === 'Camisetas'));
-  } else {
-      mostrarProductos(productosGlobales);
-  }
+document.addEventListener('DOMContentLoaded', function() {
+    // Añadiendo el listener para el dropdown del menú
+    document.getElementById('menuDropdown').addEventListener('change', (event) => {
+        const value = event.target.value;
+        if (value === 'Todos') {
+            mostrarProductos(productosGlobales);
+        } else if (value === 'Remeras') {
+            mostrarProductos(productosGlobales.filter(producto => producto.categoria === 'Remeras'));
+        } else if (value === 'Buzos') {
+            mostrarProductos(productosGlobales.filter(producto => producto.categoria === 'Buzos'));
+        } else if (value === 'Camisetas') {
+            mostrarProductos(productosGlobales.filter(producto => producto.categoria === 'Camisetas'));
+        } else if (value === 'Contactos') {
+            mostrarContacto(); 
+        } else {
+            mostrarProductos(productosGlobales);
+        }
+    });
+
+    // Función para mostrar la sección de contacto
+    function mostrarContacto() {
+        document.getElementById('mainContent').style.display = 'none';
+        document.getElementById('contactoSection').style.display = 'block';
+    }
+
+    // Añadiendo el listener para el formulario de contacto
+    const contactoForm = document.getElementById('contactoForm');
+    if (contactoForm) {
+        contactoForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            
+            const nombre = document.getElementById('nombreContacto').value; // Cambiado a 'nombreContacto'
+            const email = document.getElementById('emailContacto').value;
+            const telefono = document.getElementById('telefono').value;
+            const mensaje = document.getElementById('mensaje').value;
+
+            Swal.fire({
+                icon: 'success',
+                title: 'Mensaje Enviado',
+                text: 'Tu mensaje se ha enviado exitosamente. Te responderemos a la brevedad.',
+                confirmButtonText: 'Aceptar'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById('contactoSection').style.display = 'none';
+                    document.getElementById('mainContent').style.display = 'block'; // Regresar al contenido principal
+                }
+            });
+        });
+    }
 });
